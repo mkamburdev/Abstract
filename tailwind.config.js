ca5f52318d64d7e26f335977c530f872d2edce46
Muhammed Kambur <muhammed.kambur@dci-student.org>
2024-06-13 00:23:08 +0200

    chore: Add react-icons package to dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				purple: '#dadbf1'
+			},
+			fontFamily: {
+				sans: ['Inter', 'system-ui', 'sans-serif']
+			}
+		},
+		screens: {
+			xs: '375px',
+			md: '425px',
+			lg: '768px',
+			xl: '1024px',
+		}
+	},
+	plugins: []
 }
