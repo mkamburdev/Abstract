9ca4e64181885ff2bc4cc8f7148cf6fc6b973bf5
Muhammed Kambur <muhammed.kambur@dci-student.org>
2024-06-25 03:43:20 +0200

    Update Help component to use anchor tag for better accessibility

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 function Help() {
 	return (
 		<div className="relative">
-			<div className="bg-purple rounded-full w-12 h-12 flex justify-center items-center z-10 fixed bottom-3 right-5">
-				<div className="text-white font-bold border-2 rounded-full w-5 h-5 flex justify-center items-center">
-					?
+			<a href="#">
+				<div className="bg-purple rounded-full w-12 h-12 flex justify-center items-center z-10 fixed bottom-3 right-5">
+					<div className="text-white font-bold border-2 rounded-full w-5 h-5 flex justify-center items-center">
+						?
+					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 	)
 }
